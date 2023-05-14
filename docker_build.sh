f691abed176636b109fd8aa15e365d8e556a7325
Slava <viacheslav.plishkin@gmail.com>
2023-05-14 15:04:44 +0300

    sake fix

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo chmod 0777 . silverstripe-cache storage
-sudo chmod -R 0777 public
+sudo chmod 0777 .
+sudo chmod -R 0777 public silverstripe-cache storage
 sudo chown -R www-data public
 
 docker-compose build
@@ -9,8 +9,8 @@ docker-compose build
 docker-compose up -d db
 
 docker-compose up -d fpm
-docker-compose exec fpm bash ./vendor/bin/sake installsake
 docker-compose exec --user www-data fpm composer install --optimize-autoloader
+docker-compose exec --user root fpm bash ./vendor/bin/sake installsake
 docker-compose exec --user www-data fpm sake /dev/build flush=1
 
 docker-compose up -d websockets
